ed627583d8a600ce3d03db50822cd9c893a369ab
Bartosz Brajewski <brajewskibartosz@gmail.com>
2022-12-04 09:41:56 +0100

    optional parameter

--- Python/6.py
+++ Python/6.py
@@ -1,4 +1,5 @@
-def menu(wine, entree, desert):
+def menu(wine, entree, desert="pudding"):
     return {"wine": wine, "enttree": entree, "desert": desert}
 
 print(menu(wine="merlot",entree="chicken",desert="cake"))
+print(menu(wine="merlot",entree="chicken"))
